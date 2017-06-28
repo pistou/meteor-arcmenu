3339ab63977e6211058b5a1a79af1cc9312455f0
Pierre Skowron <skowron.pierre@gmail.com>
2017-06-28 09:10:27 +0200

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'pistou:arcmenu',
     version: '1.1.1',
     summary: 'Create arc menus with whoosh effects!',
-    git: 'https://github.com/psko/meteor-arcmenu.git',
+    git: 'https://github.com/pistou/meteor-arcmenu.git',
     documentation: 'README.md'
 });
 
